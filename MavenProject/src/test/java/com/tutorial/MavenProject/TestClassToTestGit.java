9a18e393285702b9f92ca5b9d65339fc51476d8a
Yogesh Jaiswal <yogesh.jaiswal1994@gmail.com>
2020-08-07 21:08:37 +0530

    This is for fun

---
+++ MavenProject/src/test/java/com/tutorial/MavenProject/TestClassToTestGit.java
@@ -0,0 +1,11 @@
+package com.tutorial.MavenProject;
+
+public class TestClassToTestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// It is only for fun
+
+	}
+
+}
